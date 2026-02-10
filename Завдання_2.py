d3393623a5c751b3ec7306f951b7ab50552362da
Alona Danylenko <danilen90@gmail.com>
2026-02-10 21:29:45 +0100

    Adjustments according to feedback

--- Завдання_2.py
+++ Завдання_2.py
@@ -1,13 +1,21 @@
-def get_numbers_ticket(min, max, quantity): #function returning random numbers is selected range
-    import random
+import random
+
+
+def get_numbers_ticket(min, max, quantity): 
+    """Function returning random, sorted numbers in selected range"""
     
     if min<1 or max>1000 or quantity<1 or quantity>max: #validating correctness of range and quantity
         return None
+    
     else:
         lottery_list = range(min, max+1) #creating a list with range set by parameters
         lottery = random.sample(lottery_list, k=quantity) #generating unique random numbers from the range
         lottery.sort() #sorting the list
+
+
     return lottery #returning sorted list of numbers
 
-lottery_numbers = get_numbers_ticket(1, 100, 22) #calling the function with specified parameters
-print("Your lottery numbers: ", lottery_numbers)
+
+if __name__ == "__main__":
+    lottery_numbers = get_numbers_ticket(1, 100, 22) #calling the function with specified parameters
+    print("Your lottery numbers: ", lottery_numbers)
--- Завдання_4.py
+++ Завдання_4.py
@@ -1,6 +1,12 @@
-def get_upcoming_birthdays(users):
+from datetime import datetime, timedelta, date
 
-    from datetime import datetime, timedelta, date
+
+def get_upcoming_birthdays(users):
+    """Based on a given list of ppl with birthdays, the function Returns a list of names and 
+    birthday congratulation dates for the next 7 days.
+    For birthdays falling on a weekend, congratulation date moves to the following Monday"""
+
+
     today = datetime.today().date()
     future_date = today + timedelta(days=7)  # Adding 7 days to current date
     upcoming_birthdays = [] #creating empty list
@@ -24,17 +30,18 @@ def get_upcoming_birthdays(users):
             birthday = birthday + timedelta(days=1) #if birthday is on Sunday, shift the date to Monday
             upcoming_birthdays.append({"name": user["name"], "congratulation_date": birthday.strftime("%Y.%m.%d") }) #add Monday to upcoming_birthdays list with comgratulation date
             
-            return upcoming_birthdays #return a full list 
+    
+    return upcoming_birthdays #return a full list 
 
-
-users = [
+if __name__ == "__main__":
+    users = [
     {"name": "John Doe", "birthday": "1985.01.23"},
     {"name": "Jane Smith", "birthday": "1990.01.27"},
     {"name": "Alice White", "birthday": "2010.03.12"},
     {"name": "Kate", "birthday": "2026.01.01"},
     {"name": "Mike", "birthday": "2020.02.13"},
-    {"name": "Sam", "birthday": "1999.02.08"}
+    {"name": "Sam", "birthday": "1999.02.14"}
 ]
 
-upcoming_birthdays = get_upcoming_birthdays(users)
-print("Список привітань на цьому тижні:", upcoming_birthdays)
+    upcoming_birthdays = get_upcoming_birthdays(users)
+    print("Список привітань на цьому тижні:", upcoming_birthdays)
